199ab51a4b3d953203d599a1d0b1b5e0d3b33eff
Walker Lockard <walker@projectdado.com>
2020-08-03 15:26:44 -0500

    enable cors for graphql fn

--- functions/graphql/graphql.js
+++ functions/graphql/graphql.js
@@ -48,4 +48,9 @@ const server = new ApolloServer({
   introspection: true,
 });
 
-exports.handler = server.createHandler();
+exports.handler = server.createHandler({
+  cors: {
+    origin: '*',
+    credentials: true,
+  },
+});
