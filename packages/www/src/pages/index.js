bc0343d44d14b941f4ecb6e1385c224f8ba603de
wlockiv <walker@projectdado.com>
2020-08-02 22:27:12 -0500

    theme and basic button landing page

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { Container, Heading, Button, Flex } from "theme-ui";
 
 export default (props) => (
-  <div>
-    <h1>Our Site</h1>
-  </div>
+  <Container>
+    <Flex sx={{ flexDirection: "column", padding: 3 }}>
+      <Heading as="h1">Get Stuff Done</Heading>
+      <Button
+        sx={{ mt: 2 }}
+        onClick={() => {
+          alert("Clicked");
+        }}
+      >
+        Log In
+      </Button>
+    </Flex>
+  </Container>
 );
