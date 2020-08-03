9a1c547c3d56efbd4e47f5c2695d3e200b713355
Walker Lockard <walker@projectdado.com>
2020-08-03 16:47:07 -0500

    connected fauna

--- functions/graphql/graphql.js
+++ functions/graphql/graphql.js
@@ -1,6 +1,9 @@
 const { ApolloServer, gql } = require('apollo-server-lambda');
+const faunadb = require('faunadb');
 
-// Construct a schema, using GraphQL Schema Language
+const q = faunadb.query;
+const client = new faunadb.Client({ secret: process.env.FAUNA });
+
 const typeDefs = gql`
   type Query {
     todos: [Todo]!
@@ -18,28 +21,55 @@ const typeDefs = gql`
   }
 `;
 
-const todos = {};
-let todoIndex = 0;
-
 const resolvers = {
   Query: {
-    todos: (parent, args, { user }) => {
+    todos: async (parent, args, { user }) => {
       if (!user) {
         return [];
       }
 
-      return Object.values(todos);
+      const results = await client.query(
+        q.Paginate(q.Match(q.Index('todos_by_user'), user))
+      );
+
+      return results.data.map(([ref, text, done]) => {
+        id: ref.id, text, done;
+      });
     },
   },
   Mutation: {
-    addTodo: (_, { text }) => {
-      todoIndex++;
-      const id = `key-${todoIndex}`;
-      todos[id] = { id, text, done: false };
-      return todos[id];
+    addTodo: async (_, { text }, { user }) => {
+      if (!user) {
+        throw new Error('Must be authenticated to insert todos.');
+      }
+
+      const results = await client.query(
+        q.Create(q.Collection('todos'), {
+          data: {
+            text: text,
+            done: false,
+            owner: user,
+          },
+        })
+      );
+
+      return {
+        ...results.data,
+        id: results.ref.id,
+      };
     },
-    updateTodoDone: (_, { id }) => {
-      todos[id].done = true;
+    updateTodoDone: (_, { id }, {user}) => {
+      if (!user) {
+        throw new Error('Must be authenticated to insert todos.');
+      }
+
+      const results = await client.query(
+        q.Update(q.Ref(q.Collection('todos'), id), {
+          data: {
+            done: true
+          }
+        })
+      )
       return todos[id];
     },
   },
